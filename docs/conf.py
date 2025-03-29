44a5b95edaf41e9bf4131f320f79a777fc1e1ce7
Brian St. Hilaire <brian.st-hilaire@sanctus-tech.com>
2025-03-29 21:43:28 +0100

    docs/readthedocs.io
    - Copy README into Sphinx docs, change config to support .md syntax.

--- docs/conf.py
+++ docs/conf.py
@@ -13,10 +13,12 @@ author = 'Brian St. Hilaire'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['myst_parser']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+sources_suffix = ['.rst', '.md']
 
 
 
