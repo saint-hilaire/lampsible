a44875054b8b12b66db371100c3effe53f308836
Brian St. Hilaire <brian.st-hilaire@sanctus-tech.com>
2025-04-16 18:48:35 +0200

    maintenance
    - Add 6.8 to recent valid WordPress versions, remove 6.4.* entries.
    - Bump default Joomla version to 5.3.0.

--- src/lampsible/constants.py
+++ src/lampsible/constants.py
@@ -76,15 +76,15 @@ DEFAULT_WORDPRESS_LOCALE  = 'en_US'
 RECENT_WORDPRESS_VERSIONS = [
     'latest',
     'nightly',
+    '6.8',
     '6.7.2', '6.7.1', '6.7',
     '6.6.2', '6.6.1', '6.6',
     '6.5.5', '6.5.4', '6.5.3', '6.5.2', '6.5',
-    '6.4.4', '6.4.3', '6.4.2', '6.4.1', '6.4',
 ]
 
 # Joomla
 # ------
-DEFAULT_JOOMLA_VERSION         = '5.2.5'
+DEFAULT_JOOMLA_VERSION         = '5.3.0'
 DEFAULT_JOOMLA_ADMIN_FULL_NAME = 'Sample User'
 
 # DRUPAL
